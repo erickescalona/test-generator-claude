04d6006b39a7f1fe55d562a75054d9f9780bd258
Erick Escalona <erickescalona@gmail.com>
2026-04-22 13:04:59 +0200

    Update GenerationResult.java

--- src/main/java/com/testgenerator/model/GenerationResult.java
+++ src/main/java/com/testgenerator/model/GenerationResult.java
@@ -1,20 +1,22 @@
-// GenerationResult.java
 package com.testgenerator.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class GenerationResult {
-    private String status;
-    private String message;
-    
-    public GenerationResult(String status, String message) {
-        this.status = status;
-        this.message = message;
-    }
-    
-    public String getStatus() {
-        return status;
-    }
-    
-    public String getMessage() {
-        return message;
-    }
+    private Map<String, String> generatedTests;
+    private int totalFilesProcessed;
+    private int totalTestsGenerated;
+    private Date timestamp;
+    private Map<String, List<ClassInfo>> classAnalysis;
+    private Map<String, Integer> coverageEstimates;
 }
