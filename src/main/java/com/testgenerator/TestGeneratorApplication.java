764dea227bb93df23ebd3327074ef9f11a14d274
Erick Escalona <erickescalona@gmail.com>
2026-04-22 12:38:59 +0200

    Add main Spring Boot application class

---
+++ src/main/java/com/testgenerator/TestGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.testgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGeneratorApplication.class, args);
+    }
+}
